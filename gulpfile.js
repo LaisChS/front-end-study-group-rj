50cb1e6cd1a3a1282de33ae7a78284527d3664b9
laischagas <lisachagas@yahoo.com.br>
2019-02-10 12:03:58 -0200

    feature: desafio_03: concatenando e minificando arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp'), // importando o gulp e outros plugins
     imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
 
+
+
+gulp.task('default', ['copy'], function() {
+  gulp.start('build-img', 'usemin'); 
+});
 
 gulp.task('copy', ['clean'] , function() {
   return gulp.src('src/**/*')
@@ -13,8 +23,16 @@ gulp.task('clean', function() {
   .pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
   gulp.src('dist/images/**/*') // origem - fluxo de leitura
   .pipe(imagemin())
   .pipe(gulp.dest('dist/images')); // destino - fluxo de destino
+});
+
+gulp.task('usemin', function() {
+  return gulp.src('dist/**/*.html')
+    .pipe(usemin({
+      'css' : [cssmin]
+    }))
+    .pipe(gulp.dest('dist'));
 });
